1dde674bdb943c2cb45f81b1739d7b4f41870673
Jenyded <jenik.byg@gmail.com>
2025-09-02 14:05:35 +0300

    made scene settings

--- Assets/_Project/Code/Gameplay/Enemy/EnemyDefaultStatus.cs
+++ Assets/_Project/Code/Gameplay/Enemy/EnemyDefaultStatus.cs
@@ -15,7 +15,12 @@ namespace _Project.Scripts.Gameplay.Enemy
 
         public override float GetStatValue(StatId statId)
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException(); строчка для дальнейшей логики, ниже весь метод потом удаляется
+            if (_stats.TryGetValue(statId, out var stat))
+                return stat.DefaultValue;
+
+            UnityEngine.Debug.LogWarning($"Stat {statId} not found in EnemyDefaultStatus.");
+            return 0f;
         }
 
         public override StatusEffect CloneWith(StatusEffect statusEffect)
